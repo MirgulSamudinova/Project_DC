070c65e26ee8a3f510e9a69d0eb6d647329db5cc
MirgulSamudinova <kalybekovnam@gmail.com>
2022-06-22 09:24:14 +0600

    commit for add users

---
+++ Project_DC/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Project_DC.Models
+{
+    public class Users 
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int? RoleId { get; set; }
+        public Roles Roles { get; set; }
+
+
+    }
+}
